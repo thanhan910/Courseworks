0a64112a3bb860dae9d6f7d2436447cf432fe779
thanhan910 <dnta9102002@gmail.com>
2022-06-11 04:05:30 +1000

    Update python

--- Swinburne/COS30045/data/countries/info/info.py
+++ Swinburne/COS30045/data/countries/info/info.py
@@ -1,25 +1,10 @@
-from collections import defaultdict
 import pandas as pd
-import json
 
-df_info = pd.read_excel("info/info.xlsx", sheet_name="info")
+df_info = pd.read_excel("info.xlsx", sheet_name="info")
+df_info.set_index("value", drop=False, inplace=True)
 
-df_order = pd.read_excel("info/info.xlsx", sheet_name="order")
+df_order = pd.read_excel("info.xlsx", sheet_name="order")
+df_order = df_order.apply(lambda x: x.dropna().tolist()).to_frame("order")
 
-df_info = pd.melt(df_info, id_vars=["value"], var_name="attr", value_name="data")
-
-df_info = df_info.dropna()
-
-
-data_info = defaultdict(dict)
-
-for k, v in df_info.iterrows():
-    data_info[v["value"]][v["attr"]] = v["data"]
-
-    if(v["attr"] == "class" and v["data"] == "order"):
-        data_info[v["value"]]["order"] = df_order[v["value"]].dropna().to_list()
-
-    data_info[v["value"]]["value"] = v["value"]
-
-
-open("info.json", "w").write(json.dumps(data_info, sort_keys=False))
+df = pd.merge(df_info, df_order, left_index=True, right_index=True, how="left")
+df.agg(lambda x: x.dropna().to_dict(), axis=1).to_json("info.json")
